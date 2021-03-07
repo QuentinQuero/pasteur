80fc108ff40c9514a0c38cc2aecef9497152d629
Quentin <quentin2.quero@epitech.eu>
2021-03-07 14:06:08 +0100

    add chart

--- src/app/bin-page/bin-page.component.ts
+++ src/app/bin-page/bin-page.component.ts
@@ -8,6 +8,10 @@ import {EChartsOption} from 'echarts';
 })
 export class BinPageComponent implements OnInit {
 
+  xAxisData = [];
+  data1 = [];
+  data2 = [];
+
   chartOption: EChartsOption = {
     legend: {
       data: ['ménager', 'recyclé']
@@ -22,22 +26,71 @@ export class BinPageComponent implements OnInit {
     series: [
       {
         data: [14.5, 13.6, 16.8, 14.6, 12.3, 4.7, 2.5],
-        type: 'bar',
+        type: 'line',
         color: '#5DADE2',
-        name: 'ménager'
+        name: 'ménager',
+        smooth: true
       },
       {
         data: [12.3, 6.6, 13.1, 12.2, 10.9, 3.1, 1.9],
-        type: 'bar',
+        type: 'line',
         color: '#27AE60',
-        name: 'recyclé'
+        name: 'recyclé',
+        smooth: true
       },
     ],
   };
 
+
+  chartOptionYear = {
+    legend: {
+      data: ['ménager', 'recyclé']
+    },
+    tooltip: {},
+    xAxis: {
+      data: this.xAxisData,
+      splitLine: {
+        show: false
+      }
+    },
+    yAxis: {
+    },
+    series: [{
+      name: 'ménager',
+      type: 'bar',
+      color: '#5DADE2',
+      data: this.data1,
+      emphasis: {
+        focus: 'series'
+      },
+      animationDelay: function (idx) {
+        return idx * 10;
+      }
+    }, {
+      name: 'recyclé',
+      type: 'bar',
+      color: '#27AE60',
+      data: this.data2,
+      emphasis: {
+        focus: 'series'
+      },
+      animationDelay: function (idx) {
+        return idx * 10 + 100;
+      }
+    }],
+    animationEasing: 'elasticOut',
+    animationDelayUpdate: function (idx) {
+      return idx * 5;
+    }
+  };
   constructor() { }
 
   ngOnInit(): void {
+    for (var i = 100; i > 30; i--) {
+      // this.xAxisData.push(new Date().getDay());
+      this.data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+      this.data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    }
   }
 
 
