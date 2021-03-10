a3763b00776d9b2e18f5b3669a5c526c71187305
Quentin <quentin2.quero@epitech.eu>
2021-03-10 19:05:20 +0100

    add chart don-reemploi

--- src/app/pages/materiaux-page/materiaux-page.component.ts
+++ src/app/pages/materiaux-page/materiaux-page.component.ts
@@ -102,4 +102,45 @@ export class MateriauxPageComponent implements OnInit {
       }
     ]
   };
+
+  option = {
+    tooltip: {},
+    legend: {
+      data: ['Dons', 'Réemploi']
+    },
+    radar: {
+      // shape: 'circle',
+      name: {
+        textStyle: {
+          color: '#fff',
+          backgroundColor: '#999',
+          borderRadius: 3,
+          padding: [3, 5]
+        }
+      },
+      indicator: [
+        { name: 'Bois', max: 6500},
+        { name: 'Metal', max: 16000},
+        { name: 'Plastiques', max: 30000},
+        { name: 'Meubles', max: 38000},
+        { name: 'Peintures', max: 52000},
+        { name: 'Autres', max: 25000}
+      ]
+    },
+    series: [{
+      name: 'Don et réemploi',
+      type: 'radar',
+      // areaStyle: {normal: {}},
+      data: [
+        {
+          value: [4300, 10000, 28000, 35000, 50000, 19000],
+          name: 'Dons'
+        },
+        {
+          value: [5000, 14000, 28000, 31000, 42000, 21000],
+          name: 'Réemploi'
+        }
+      ]
+    }]
+  };
 }
